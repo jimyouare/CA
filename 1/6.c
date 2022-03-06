0d69e7f255da7c3f1b4c0b2976048fe55d146dd8
Tobi <jimyouare@gmail.com>
2022-03-06 13:49:38 +0100

    6

--- 1/6.c
+++ 1/6.c
@@ -5,7 +5,7 @@ int main(int argc, const char * argv[]) {
         char a[10];
         char b;
         char c;
-        short intd;
+        short int d;
     }myStruct;
 
     union {
@@ -14,6 +14,10 @@ int main(int argc, const char * argv[]) {
         short int d[4];
     } myUnion;
 
+    //struct: each element gets its own disk space -> 14
+    // char a[10]: 10 bytes + char b 1 byte + char c 1 byte + short int 2 bytes
+    //union: disk space is defined by biggest element -> 12
+    // char a[12]: 12 bytes
     printf("%i\n", sizeof (myStruct));
     printf("%i\n", sizeof (myUnion));
 }
