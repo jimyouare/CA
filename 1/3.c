d3a53a4e8ee19f75f69695e3fa5c2b3306b685d4
Tobi <jimyouare@gmail.com>
2022-03-06 14:21:42 +0100

    ex 3 more

--- 1/3.c
+++ 1/3.c
@@ -5,20 +5,27 @@ int main(int argc, const char * argv[]) {
     printf("Question 3\n");
     long a = 1234567890; /* Hex: 499602d2 */
     long b = 1000000000; /* Hex: 3b9aca00 */
-
-    //pointer to b
+    printf("a %x\n",a);
+    printf("b %x\n",b);
+    // void pointer to b
     void*p=&b;
 
-    //print address of b since p points to this address
+    // print address of b since p points to this address
     printf("%x\n", p);
-    printf("Address of b: %p\n", &b);
+    //printf("Address of b: %p\n", &b);
 
-    //pointer p is incremented and casted to a pointer of type long
+    // void pointer p is incremented and cast to a pointer of type long
+    // prints b
     printf("%x\n",*(long*)p++);
 
-
+    // void pointer p is incremented and cast to a pointer of type char
+    // prints
     printf("%x\n",*(char*)p++);
+
+    //
     printf("%x\n",*(unsigned char*)p++);
+
+    //
     printf("%x\n", p);
 
 
