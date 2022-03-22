1dd9beef0bc8e6899e31dcd58489c3126e657d52
Esther Hoeun Yu <hoeuyu@student.ethz.ch>
2022-03-22 13:51:51 +0100

    Create Organisation.c
    https://www.slideshare.net/WaqarJamil5/06-mipsisa

---
+++ Serie 3/Der Aufrufer/Organisation.c
@@ -0,0 +1,27 @@
+Prozessor
+
+Haupspeicher 
+
+read/write addr 
+
+read data 
+
+write data 
+
+2^30 words 
+
+0...1100
+  0.1000
+  
+  
+  MIPS ISA bis jetzt 
+  
+  Category: 
+Instr: 
+
+Op Code/ Funct
+
+Example 
+
+Meaning 
+
