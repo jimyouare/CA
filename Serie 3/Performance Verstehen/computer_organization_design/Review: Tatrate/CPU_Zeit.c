b10a23117473c96183899e431c01ce2852bcef2e
Esther Hoeun Yu <hoeuyu@student.ethz.ch>
2022-03-22 14:30:30 +0100

    Create CPU_Zeit.c

---
+++ Serie 3/Performance Verstehen/computer_organization_design/Review: Tatrate/CPU_Zeit.c
@@ -0,0 +1,9 @@
+CPU Zeit = IC x CPI x Taktperiode 
+
+CPU Zeit = IC x CPI / Taktrate 
+
+Diese Gleichungen zeigen die drei Schlüsselfaktoren welche Einfluss auf die Performance haben. 
+  
+  Messen der CPU Zeit durch ausführen dort. 
+  
+  
