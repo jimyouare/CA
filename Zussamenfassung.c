00c51ca3167b3c48739caeb32578ae7b887cc6a4
Esther Hoeun Yu <hoeuyu@student.ethz.ch>
2022-03-22 14:45:11 +0100

    Create Zussamenfassung.c

---
+++ Zussamenfassung.c
@@ -0,0 +1,14 @@
+Evaluating ISAs 
+
+Metriken Zur Entwurfroszeit: 
+Kann das implementiert werden, in welcher Zeit, zu welchen Kosten?
+  
+  - Kann das programmiert werden?
+  Leichtere Kompilierung? 
+  
+  # Statische Metriken: 
+  Wie viele Bytes benötigt das Programm im Hauptspeicher? 
+  
+  # Dynamische Metriken: 
+  Wie viele Befehle werden für ein Programm ausgeführt? 
+  Wie viele Bytes muss der Prozessor laden um das Programm auszuführen ?
