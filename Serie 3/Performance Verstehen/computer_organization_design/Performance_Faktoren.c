c3e89a40c310dec036d92b8008bdfa3a08e7c14a
Esther Hoeun Yu <hoeuyu@student.ethz.ch>
2022-03-22 14:24:43 +0100

    Create Performance_Faktoren.c

---
+++ Serie 3/Performance Verstehen/computer_organization_design/Performance_Faktoren.c
@@ -0,0 +1,5 @@
+Müssen unterscheiden zwischen total verstrichener Zeit und Zeit welhe für ein Programm verwendet wurde
+
+CPU Ausführungszeit (CPU Zeit) Zeit welche die CPU an einem Programm arbeitet 
+
+Beinhaltet keine IO Wartezeiten machen können. 
