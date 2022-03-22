1ade2ee2fb4ee23a3a0a0552ba3c302f9eb0abab
Esther Hoeun Yu <hoeuyu@student.ethz.ch>
2022-03-22 13:41:54 +0100

    Create Funktionsende.c

---
+++ Serie 3/Der Aufrufer/Funktionsende.c
@@ -0,0 +1,3 @@
+@ was ist eine Funktionsende
+
+In der Mathematik stellt eine Funktion eine Zuordnung zwischen zwei Mengen dar. Jedem Element der einen Menge wird genau ein Element der anderen Menge zuordnet. Schreibweisen Funktion: Im Beispiel hat jeder Schokoriegel 0,50 Euro gekostet.
